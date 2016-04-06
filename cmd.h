49ebdb6a59389f5b4d5b4a9d43c4ad50a9318851
laozhu <shuiyun_5182604@163.com>
2016-04-06 16:43:01 +0800

    init kline service

--- cmd.h
+++ cmd.h
@@ -7,5 +7,6 @@ using namespace std;
 const string CMD_MSG_SHUTDOWN = "0";
 const string CMD_MSG_START = "1";
 const string CMD_MSG_TRADE_FOK = "2";
+const string CMD_MSG_MARKET_DATA = "3";
 
 #endif
---
+++ kLine/main.cpp
@@ -0,0 +1,45 @@
+#include "../iniReader/iniReader.h"
+#include "../socket.h"
+#include "../lib.h"
+#include "../cmd.h"
+#include <string>
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    parseIniFile("config.ini");
+
+    // int          traderSrvPort = getOptionToInt("trader_srv_port");
+    // const char * traderSrvIp   = getOptionToChar("trader_srv_ip");
+    int          kLineSrvPort  = getOptionToInt("k_line_srv_port");
+
+    int sfd = getSSocket(kLineSrvPort);
+
+    char buff[1024];
+    string msgLine, msg;
+    vector<string> params;
+    int cfd, n;
+    while (true) {
+        if ((cfd = accept(sfd, (struct sockaddr *)NULL, NULL)) == -1) {
+            cout << "accept socket error: " << strerror(errno) <<  endl;
+            continue;
+        }
+        n = recv(cfd, buff, 1024, 0);
+        close(cfd);
+        if (n == 0) continue;
+
+        buff[n] = '\0';
+        msgLine = string(buff);
+        msgLine = trim(msgLine);
+        params = Lib::split(msgLine, "_");
+        msg = params[0];
+        if (msg == CMD_MSG_SHUTDOWN) {
+            break;
+        }
+        if (msg == CMD_MSG_MARKET_DATA) {
+            // todo
+        }
+    }
+    close(sfd);
+    return 0;
+}
