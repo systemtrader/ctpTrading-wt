70971bc5143bad31b39f7fee260d778b26d7bc8c
laozhu <shuiyun_5182604@163.com>
2016-05-21 22:12:41 +0800

    change find.sh

--- bin/find.sh
+++ bin/find.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #
 #
-ps -ef | grep marketSrv | grep -v grep
-ps -ef | grep kLineSrv | grep -v grep
-ps -ef | grep tradeLogicSrv | grep -v grep
-ps -ef | grep tradeStrategySrv | grep -v grep
-ps -ef | grep tradeSrv | grep -v grep
+ps aux | grep marketSrv | grep -v grep
+ps aux | grep kLineSrv | grep -v grep
+ps aux | grep tradeLogicSrv | grep -v grep
+ps aux | grep tradeStrategySrv | grep -v grep
+ps aux | grep tradeSrv | grep -v grep
 
-ps -ef | grep tradeStrategyAfter | grep -v grep
+ps aux | grep tradeStrategyAfter | grep -v grep
 
-ps -ef | grep tick.php | grep -v grep
-ps -ef | grep readHistoryKLine.php | grep -v grep
-ps -ef | grep order.php | grep -v grep
-ps -ef | grep kLine.php | grep -v grep
+ps aux | grep tick.php | grep -v grep
+ps aux | grep readHistoryKLine.php | grep -v grep
+ps aux | grep order.php | grep -v grep
+ps aux | grep kLine.php | grep -v grep
